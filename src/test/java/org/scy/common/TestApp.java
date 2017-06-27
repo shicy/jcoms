dcadfe295b6d25f54bdfbdbdc4af8ebed234c908
shicy <shicy85@163.com>
2017-06-27 15:27:53 +0800

    Springboot测试应用

---
+++ src/test/java/org/scy/common/TestApp.java
@@ -0,0 +1,16 @@
+package org.scy.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试应用
+ * Created by shicy on 2017/6/22.
+ */
+@SpringBootApplication
+public class TestApp {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("run test app");
+    }
+
+}
