260cf262f5501160cb498740b7dae0ca5189002d
shicy <shicy85@163.com>
2017-06-27 15:29:48 +0800

    数据库更新

--- src/main/java/org/scy/common/ds/DbUpgrade.java
+++ src/main/java/org/scy/common/ds/DbUpgrade.java
@@ -1,5 +1,9 @@
 package org.scy.common.ds;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.io.File;
 
 /**
@@ -11,23 +15,53 @@ import java.io.File;
  */
 public class DbUpgrade extends Thread {
 
+    private Logger logger = LoggerFactory.getLogger(DbUpgrade.class);
+
+    // 数据库操作
+    private JdbcTemplate jdbcTemplate;
+    // 脚本所在资源文件目录
+    private String resourceFilePath;
+    // 脚本文件集，与 resourceFilePath 是2种不同的处理方案
+    private File[] scriptFiles;
+
     // 数据库更新监听
-    private DbUpgradeListener listener = null;
+    private DbUpgradeListener listener;
 
-    public DbUpgrade() {
-
+    /**
+     * 构造方法，根据资源文件夹更新数据库
+     * 遍历资源脚本文件（“.sql”文件，包含子目录），按照文件版本规则排序，顺序执行
+     * 其中，版本规则是：[类型编号.大版本.小版本]，后面跟文件名称，如果没有版本信息默认按文件名称排序
+     * @param jdbcTemplate 数据库链接方法
+     * @param resourceFilePath 脚本所在资源文件目录
+     */
+    public DbUpgrade(JdbcTemplate jdbcTemplate, String resourceFilePath) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.resourceFilePath = resourceFilePath;
     }
 
-    public DbUpgrade(String[] scripts) {
-
-    }
-
-    public DbUpgrade(File[] scripts) {
+    /**
+     * 构造方法，指定脚本文件对象集，按顺序执行脚本
+     * @param jdbcTemplate 数据库链接方法
+     * @param scriptFiles 脚本文件集
+     */
+    public DbUpgrade(JdbcTemplate jdbcTemplate, File[] scriptFiles) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.scriptFiles = scriptFiles;
     }
 
     @Override
     public void run() {
-        System.out.println("开始数据库升级...");
+        if (listener == null)
+            listener = new DbUpgradeListener();
+
+        if (jdbcTemplate == null)
+            listener.fail("未知数据库连接");
+
+        listener.before();
+    }
+
+    private void initDatabase() {
+
     }
 
     /**
@@ -41,7 +75,29 @@ public class DbUpgrade extends Thread {
     /**
      * 数据库更新监听类
      */
-    public static interface DbUpgradeListener {
+    public static class DbUpgradeListener {
+
+        protected Logger logger = LoggerFactory.getLogger(DbUpgradeListener.class);
+
+        public void before() {
+            logger.info("开始数据库更新...");
+            // do something before upgrade
+        }
+
+        public void doing(String fileName) {
+            logger.info("正在执行脚本：" + fileName);
+            // do something when execute scripts
+        }
+
+        public void after() {
+            logger.info("数据库更新已完成！");
+            // do something after upgrade
+        }
+
+        public void fail(String errorMsg) {
+            logger.error("数据库更新失败：" + errorMsg);
+            throw new RuntimeException(errorMsg);
+        }
 
     }
 }
--- src/test/java/org/scy/common/ds/DbUpgradeTest.java
+++ src/test/java/org/scy/common/ds/DbUpgradeTest.java
@@ -1,15 +1,31 @@
 package org.scy.common.ds;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * 数据库更新测试
  * Created by shicy on 2017/6/9.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import({TestDataSourceBeanDefinition.class})
 public class DbUpgradeTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     public void doTest() {
-        new DbUpgrade();
+        DbUpgrade upgrade = new DbUpgrade(jdbcTemplate, "org/scy/common/test/scripts");
+        upgrade.setListener(new DbUpgrade.DbUpgradeListener() {
+
+        });
+        upgrade.start();
     }
 }
