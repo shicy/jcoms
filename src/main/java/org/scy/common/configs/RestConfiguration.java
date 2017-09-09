8049802795aff18c7850caa69192d403a7c2cd15
shicy <shicy85@163.com>
2017-09-09 22:21:29 +0800

    新增：REST请求配置

---
+++ src/main/java/org/scy/common/configs/RestConfiguration.java
@@ -0,0 +1,45 @@
+package org.scy.common.configs;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Rest 请求配置
+ * Created by shicy on 2017/9/8.
+ */
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean({RestOperations.class, RestTemplate.class})
+    public RestOperations restOperations() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(5000);
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        // 使用 utf-8 编码集的 conver 替换默认的 conver（默认的 string conver 的编码集为 "ISO-8859-1"）
+        List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
+        Iterator<HttpMessageConverter<?>> iterator = messageConverters.iterator();
+        while (iterator.hasNext()) {
+            HttpMessageConverter converter = iterator.next();
+            if (converter instanceof StringHttpMessageConverter) {
+                iterator.remove();
+            }
+        }
+        messageConverters.add(new StringHttpMessageConverter(Charset.forName("UTF-8")));
+
+        return restTemplate;
+    }
+}
