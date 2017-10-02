44c5173621678424a4ae3bfc5e13f8da47e17de5
shicy <shicy85@163.com>
2017-10-02 22:37:40 +0800

    常量变更，添加错误编码

--- src/main/java/org/scy/common/Const.java
+++ src/main/java/org/scy/common/Const.java
@@ -13,16 +13,27 @@ public final class Const {
     // 无效
     public final static short DISABLED = 0;
 
+    // 平台帐号
+    public final static String PLATFORM_CODE = "1000000000000000";
+
     // 参数错误
     public final static short MSG_CODE_PARAMINVALID  = 1000;
 
     // 参数缺失
     public final static short MSG_CODE_PARAMMISSING = 1001;
 
+    // 参数格式错误
+    public final static short MSG_CODE_PARAMFORMATERROR = 1002;
+
+    public final static short MSG_CODE_DUPLICATED = 1003;
+
     // 服务拒绝访问或连接失败
     public final static short MSG_CODE_SERVERREFUSED = 1010;
 
     // 当前帐户错误，不存在或密码错误
     public final static short MSG_CODE_ACCOUNTERROR = 1100;
+
+    // 没有操作权限
+    public final static short MSG_CODE_NOPERMISSION = 1101;
 
 }
