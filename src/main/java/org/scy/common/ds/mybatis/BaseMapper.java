03abc31be2dcf022bc2d117abd4b1e651d5433d2
shicy <shicy85@163.com>
2017-10-13 23:10:41 +0800

    BaseMapper接口设计

--- src/main/java/org/scy/common/ds/mybatis/BaseMapper.java
+++ src/main/java/org/scy/common/ds/mybatis/BaseMapper.java
@@ -1,8 +1,25 @@
 package org.scy.common.ds.mybatis;
 
+import org.scy.common.ds.query.Selector;
+
+import java.util.List;
+
 /**
  * Mybatis 映射文件配置基类
  * Created by shicy on 2017/5/15.
  */
 public interface BaseMapper<T> {
+
+    T getById(int id);
+
+    int add(T model);
+
+    int update(T model);
+
+    int delete(T model);
+    int deleteById(int id);
+
+    List<T> find(Selector selector);
+    int countFind(Selector selector);
+
 }
