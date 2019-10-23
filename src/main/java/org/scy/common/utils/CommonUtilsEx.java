fea3d9be390101b0142d6d3f2e994d3b2ecd628c
shicy <shicy85@163.com>
2019-10-23 12:49:13 +0800

    手机号码验证修改，验证条件放宽，1开头的11位数字即可

--- src/main/java/org/scy/common/utils/CommonUtilsEx.java
+++ src/main/java/org/scy/common/utils/CommonUtilsEx.java
@@ -42,7 +42,8 @@ public abstract class CommonUtilsEx {
      */
     public static boolean checkMobile(String mobile) {
         if (StringUtils.isNotBlank(mobile)) {
-            String regExp = "^1((3[0-9])|(4[57])|(5[0-35-9])|(7[0135678])|(8[0-9]))\\d{8}$";
+//            String regExp = "^1((3[0-9])|(4[57])|(5[0-35-9])|(7[0135678])|(8[0-9]))\\d{8}$";
+            String regExp = "^1\\d{10}$";
             Pattern pattern = Pattern.compile(regExp);
             Matcher matcher = pattern.matcher(mobile);
             return matcher.matches();
