663cb7a4a940d6f2d4af74266798a79379895c0d
shicy <shicy85@163.com>
2020-01-20 17:55:35 +0800

    添加 BaseController.getAppVersion() 方法

--- src/main/java/org/scy/common/web/controller/BaseController.java
+++ src/main/java/org/scy/common/web/controller/BaseController.java
@@ -1,8 +1,20 @@
 package org.scy.common.web.controller;
 
+import org.scy.common.BaseApplication;
+import org.scy.common.configs.AppConfigs;
+
 /**
  * 控制器基类，实现控制器公用方法
  * Created by shicy on 2017/5/11.
  */
 public abstract class BaseController {
+
+    /**
+     * 获取版本号
+     */
+    protected String getAppVersion() {
+        AppConfigs appConfigs = BaseApplication.getAppConfigs();
+        return appConfigs.getVersion();
+    }
+
 }
