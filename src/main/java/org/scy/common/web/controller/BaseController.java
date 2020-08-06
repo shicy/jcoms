2210ee5dc9087d2af484f60bc9a25691e72ca5b5
shicy <shicy85@163.com>
2020-08-07 06:48:03 +0800

    BaseController 添加当前运行环境

--- src/main/java/org/scy/common/web/controller/BaseController.java
+++ src/main/java/org/scy/common/web/controller/BaseController.java
@@ -2,12 +2,24 @@ package org.scy.common.web.controller;
 
 import org.scy.common.BaseApplication;
 import org.scy.common.configs.AppConfigs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 
 /**
  * 控制器基类，实现控制器公用方法
  * Created by shicy on 2017/5/11.
  */
-public abstract class BaseController {
+@Controller
+public class BaseController {
+
+    @Autowired
+    protected Environment env;
+
+    protected boolean isDev() {
+        String _env = env.getActiveProfiles()[0];
+        return "dev".equals(_env);
+    }
 
     /**
      * 获取版本号
