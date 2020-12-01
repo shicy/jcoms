757f157fa770b65edcd10af616b6c1f386bcd56c
shicy <shicy85@163.com>
2020-12-01 18:05:58 +0800

    BaseService.getUserId()

--- src/main/java/org/scy/common/web/service/BaseService.java
+++ src/main/java/org/scy/common/web/service/BaseService.java
@@ -1,8 +1,15 @@
 package org.scy.common.web.service;
 
+import org.scy.common.web.session.SessionManager;
+
 /**
  * 服务类基类
  * Created by shicy on 2017/5/12.
  */
 public class BaseService {
+
+    protected int getUserId() {
+        return SessionManager.getUserId();
+    }
+
 }
